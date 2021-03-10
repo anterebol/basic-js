4827d9af4e5250c9736f4dd7dbb0bb2c7677bc05
unknown <anterebol@gmail.com>
2021-03-10 15:10:37 +0300

    feat: add submit

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,22 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity)  {
+    if (sampleActivity === 'undefiend') {
+      return false;
+    }
+        else if (sampleActivity < 0) {
+          return false;
+        }
+        else if (Number(sampleActivity) > 0) {
+        var t;
+      let y = 0.693;
+      let k;
+         k = y / HALF_LIFE_PERIOD;
+      t = (Math.log(MODERN_ACTIVITY / sampleActivity)) / k;
+      return Math.ceil(t);  
+        } 
+        else  {
+          return false;
+        }
+      }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  let new_massiv = arr;
+  let i = 0;
+  for (i; i < arr.length; i++) {
+    if (arr[i] === '--discard-next'){
+      new_massiv.splice(i, 2);
+    }
+    else if (arr[i] === '--discard-prev'){
+      new_massiv.splice(i-1, 2)
+    }
+    else if (arr[i] === '--double-next'){
+      new_massiv.splice(i, 1, arr[i+1]);
+    }
+    else if (arr[i] === '--double-prev'){
+      new_massiv.splice(i, 1, arr[i-1]);
+    }
+      }
+  for (i; i < new_massiv.length; i++) {
+    if (typeof(new_massiv[i]) !== 'number') {
+      new_massiv.splice(i, 1);
+    }
+  }
+      return new_massiv;
 };
